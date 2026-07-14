5ca374a10919a5df370ce04fd4ea163575042ac8
AedanThornton <ajt1111@outlook.com>
2026-07-14 16:19:40 -0500

    New: post-generator now does gates and transforming cards

--- post-generator.js
+++ post-generator.js
@@ -1,3 +1,4 @@
+const { sign } = require('crypto');
 const fs = require('fs');
 
 function cycleToNumber(cycleStr) {
@@ -13,6 +14,38 @@ function cycleToNumber(cycleStr) {
   return convert[cycleStr] || "N/A"
 }
 
+function getType(card, side = "") {
+  return card["slot" + side].includes("Hand") ? "Weapon" : card["slot" + side]
+}
+
+function getDStats(card, side = "") {
+  return card["defensiveStatistics" + side].map((stat) => `* ${stat.type} ${stat.amount}`).join("\n")
+}
+
+function getAbilities(card, side = "") {
+  return card["abilities" + side].map(
+    (sentence) => {
+      let output = "* "
+      output = output + (sentence.costs ? sentence.costs.join(" ") + " " : "")
+      output = output + sentence.abilityText.map((ability) => ability.value).join("")
+      return output
+    }).join("\n")
+}
+
+function getGatedAbilities(card, side = "") {
+  return card["gatedAbilities" + side].map(
+    (gatedAbility) => {
+      let output = "* <" + gatedAbility.gate + " / " + gatedAbility.value + "> "
+      output = output + gatedAbility.abilities.map((sentence) => {
+        let output = ""
+        output = output + (sentence.costs ? sentence.costs.join(" ") + " " : "")
+        output = output + sentence.abilityText.map((ability) => ability.value).join("")
+        return output
+      }).join(". ")
+      return output
+    }).join("\n")
+}
+
 function createPost({ day }) {
   fs.readFile('./data/JSON/gearData.json', 'utf8', (err, data) => {
     if (err) {
@@ -24,25 +57,17 @@ function createPost({ day }) {
       parseInt(a.cardIDs[0].slice(2), 10) - parseInt(b.cardIDs[0].slice(2), 10)
     );
     const todaysCard = sortedGearCards[day - 1]
-
-    const gearType = todaysCard.slot.includes("Hand") ? "Weapon" : todaysCard.slot
-    const defStats = todaysCard.defensiveStatistics.map((stat) => `* ${stat.type} ${stat.amount}`).join("\n")
-    const abilities = todaysCard.abilities.map(
-      (sentence) => {
-        let output = "* "
-        output = output + (sentence.costs ? sentence.costs.join(" ") + " " : "")
-        output = output + sentence.abilityText.map((ability) => ability.value).join("")
-        return output
-      }).join("\n")
+    const isTransforming = !!todaysCard.transformsInto
 
     console.log(`
 Day ${day} 
 
 >>> __**${todaysCard.name}**__
+**Acquired From**: ${todaysCard.acquisition}
 **Power Level**: ${cycleToNumber(todaysCard.cycle)}
 **Power Tier**: -
 
-**Gear type**: ${gearType}
+**Gear type**: ${getType(todaysCard)}
 **Slots used**: ${todaysCard.slot}
 __Offensive Statistics__:
 ${Object.keys(todaysCard.offensiveStatistics).length > 0 ?
@@ -53,16 +78,37 @@ ${Object.keys(todaysCard.offensiveStatistics).length > 0 ?
       }
 
 __Defensive Statistics__:
-${defStats || "* None"}
+${getDStats(todaysCard) || "* None"}
 
 __Abilities__:
-${abilities || "* None"}
+${getAbilities(todaysCard) || "* None"}${"\n" + getGatedAbilities(todaysCard) || ""}
 
-**Flavor Text**: *${todaysCard.flavor}*
+${!isTransforming ? `**Flavor Text**: *${todaysCard.flavor}*`: ""}
 **Traits**: ${todaysCard.traits.join(", ")}
 
+${isTransforming ? `
+__**${todaysCard.name2}**__
+**Gear type**: ${getType(todaysCard, "2")}
+**Slots used**: ${todaysCard.slot2}
+__Offensive Statistics__:
+${Object.keys(todaysCard.offensiveStatistics).length > 0 ?
+        `* **Attack Dice**: ${todaysCard.offensiveStatistics2.attackDice}
+* **To-Hit bonus**: ${todaysCard.offensiveStatistics2.precision}
+* **Power dice**: ${JSON.stringify(todaysCard.offensiveStatistics2.power)}` :
+        "* None"
+      }
+
+__Defensive Statistics__:
+${getDStats(todaysCard, "2") || "* None"}
+
+__Abilities__:
+${getAbilities(todaysCard, "2") || "* None"}${"\n" + getGatedAbilities(todaysCard, "2") || ""}
+
+**Traits**: ${todaysCard.traits2.join(", ")}
+  
+  `: ""}
 `)
   });
 }
 
-createPost({ day: 86 })
+createPost({ day: 91 })
